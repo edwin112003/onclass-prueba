0ac86094a44c32d9a646d81f0f14cc882fa02ffa
RodriguezJimenezRicardo <rodriguez.jimenez.jose.ricardo027@gmail.com>
2021-01-08 17:38:09 -0600

    pinches llaves

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database:{
         host: 'us-cdbr-east-02.cleardb.com',
-        database:'heroku_103f41760a8311b',
-        user:'bcc4b7c704c7d0',
-        password:'85ffbbd0'
+        database:'heroku_6fb09b5011f095a',
+        user:'b6bcd30868f925',
+        password:'5336b416'
     }
 };
