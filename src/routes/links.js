cf807240e4a7059b69d67be4ca737c6fbd893981
RodriguezJimenezRicardo <rodriguez.jimenez.jose.ricardo027@gmail.com>
2021-01-08 19:13:03 -0600

    dads

--- src/routes/links.js
+++ src/routes/links.js
@@ -410,7 +410,7 @@ router.get('/clase_tomar_nota',isLoggedIn, async(req,res)=>{
         let dia = fecha.getDay();
         let nombre_clase = '';
         let clase;
-        let hora=19;
+         hora=19;
         let contador = 0;
         if(dia==0) dia=7;        
         const clase_actual = await pool.query('call GetClasDia (?,?)', [5, req.app.locals.user.id_usuario]);
